8fb0fbb24a71844e481cdd2393b4151b66c95ccc
Alfonso Alvarez <ponchoalv@gmail.com>
2019-06-10 11:58:19 -0300

    step2 to step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,9 +6,34 @@ const Button = ({label, handleClick}) => (
   <button onClick={handleClick}>{label}</button>
 )
 
-const Stat = ({label, quantity}) => (
-  <p>{label + ' ' + quantity}</p>
-)
+const Statistic = ({label, quantity, percent=false}) => {
+  if(percent){
+    return (<p>{label + ' ' + quantity} %</p>)
+  }
+  return (<p>{label + ' ' + quantity}</p>)
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = good / total * 100
+
+  if(total === 0) {
+    return (<p>No feedback given</p>)
+  }
+
+  return (
+    <>
+    <Statistic label='good' quantity={good}/>
+    <Statistic label='neutral' quantity={neutral}/>
+    <Statistic label='bad' quantity={bad}/>
+    <Statistic label='all' quantity={total}/>
+    <Statistic label='average' quantity={average}/>
+    <Statistic label='positive' quantity={positive} percent/>
+    </>
+  )
+
+}
 
 const App = () => {
   // save clicks of each button to own state
@@ -26,10 +51,8 @@ const App = () => {
       <Button label='good' handleClick={incrementGood}/>
       <Button label='neutral' handleClick={incrementNeutral}/>
       <Button label='bad' handleClick={incrementBad}/>
-      <h1>statics</h1>
-      <Stat label='good' quantity={good}/>
-      <Stat label='neutral' quantity={neutral}/>
-      <Stat label='bad' quantity={bad}/>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
